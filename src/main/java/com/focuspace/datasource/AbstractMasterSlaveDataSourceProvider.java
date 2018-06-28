f44f8b3391412b7294f6d01dc23a8a69ad3b8dd1
fengde <derek.feng@mkcorp.com>
2018-06-28 13:36:34 +0800

    clean git

--- src/main/java/com/focuspace/datasource/AbstractMasterSlaveDataSourceProvider.java
+++ src/main/java/com/focuspace/datasource/AbstractMasterSlaveDataSourceProvider.java
@@ -1,8 +1,6 @@
 package com.focuspace.datasource;
 
-import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 
 import javax.sql.DataSource;
 
@@ -13,23 +11,5 @@ public abstract class AbstractMasterSlaveDataSourceProvider implements MasterSla
         Class<? extends DataSource> type = properties.getType();
 
         return properties.initializeDataSourceBuilder().build();
-
-//        if (type == null) {
-//            try {
-//                Class.forName("com.zaxxer.hikari.HikariDataSource");
-//                return createHikariDataSource(properties);
-//            } catch (ClassNotFoundException e) {
-//                log.debug("dynamic not found HikariDataSource");
-//            }
-//            throw new RuntimeException("please set master and slave type like spring.dynamic.datasource.master.type");
-//        } else {
-//            return properties.initializeDataSourceBuilder().build();
-//        }
     }
-
-
-//    private DataSource createHikariDataSource(DataSourceProperties properties) {
-//        properties.setType(HikariDataSource.class);
-//        return properties.initializeDataSourceBuilder().build();
-//    }
 }

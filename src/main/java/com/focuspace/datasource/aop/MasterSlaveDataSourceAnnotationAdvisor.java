205775da5548565fb23c5ec78de1169dcde37a38
fengde <derek.feng@mkcorp.com>
2018-07-02 16:15:03 +0800

    Revert "aop not working, revert it"
    This reverts commit 02b766e5aa018c8919beaf393d673fa2854538dd.

--- src/main/java/com/focuspace/datasource/aop/MasterSlaveDataSourceAnnotationAdvisor.java
+++ src/main/java/com/focuspace/datasource/aop/MasterSlaveDataSourceAnnotationAdvisor.java
@@ -1,19 +1,19 @@
-package com.focuspace.datasource.aop;
-
-import com.focuspace.datasource.MasterSlave;
-import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
-
-import java.lang.reflect.Method;
-
-/**
- * @author fengde
- */
-public class MasterSlaveDataSourceAnnotationAdvisor extends StaticMethodMatcherPointcutAdvisor {
-    @Override
-    public boolean matches(Method method, Class<?> aClass) {
-        if (method.isAnnotationPresent(MasterSlave.class)) {
-            return true;
-        }
-        return false;
-    }
-}
+//package com.focuspace.datasource.aop;
+//
+//import com.focuspace.datasource.MasterSlave;
+//import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
+//
+//import java.lang.reflect.Method;
+//
+///**
+// * @author fengde
+// */
+//public class MasterSlaveDataSourceAnnotationAdvisor extends StaticMethodMatcherPointcutAdvisor {
+//    @Override
+//    public boolean matches(Method method, Class<?> aClass) {
+//        if (method.isAnnotationPresent(MasterSlave.class)) {
+//            return true;
+//        }
+//        return false;
+//    }
+//}
